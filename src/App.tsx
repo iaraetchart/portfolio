7a09df0af83d8ba42575faf901ca02e60d9859ed
Iara Etchart <iaraetchart@gmail.com>
2024-11-19 13:28:20 -0300

    Add Vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar/Navbar'
 import Skills from './components/Skills/Skills'
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact.tsx'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
     <Skills/>
     <Projects/>
     <Contact/>
+    <Analytics/>
     </>
   )
 }
